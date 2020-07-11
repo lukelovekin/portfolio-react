d5d9345d5b20577f418466cd37ef461f461e3945
Luke <lukelovekin@gmail.com>
2020-07-11 20:34:19 +1000

    Updates

---
+++ precache-manifest.9888a27ee1280ab7dde2a5c28b4e5538.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb35816831ca36c48bae1af0675fdbb4",
+    "url": "/portfolio-react/index.html"
+  },
+  {
+    "revision": "66883bdba6f0c763fe51",
+    "url": "/portfolio-react/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "c03fe905708d97dc949b",
+    "url": "/portfolio-react/static/css/main.61be5816.chunk.css"
+  },
+  {
+    "revision": "66883bdba6f0c763fe51",
+    "url": "/portfolio-react/static/js/2.e19060b0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio-react/static/js/2.e19060b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c03fe905708d97dc949b",
+    "url": "/portfolio-react/static/js/main.a9c35706.chunk.js"
+  },
+  {
+    "revision": "f16589a8831e993e10da",
+    "url": "/portfolio-react/static/js/runtime-main.5febc6f1.js"
+  },
+  {
+    "revision": "e8e63f06a9c035a61f95f2c416b7bfe8",
+    "url": "/portfolio-react/static/media/back.e8e63f06.jpg"
+  },
+  {
+    "revision": "a32106b96b0e4840e2abd48f8da835bc",
+    "url": "/portfolio-react/static/media/face.a32106b9.png"
+  },
+  {
+    "revision": "f07b8c3bf7507d9f0f484bbe1f0b8c33",
+    "url": "/portfolio-react/static/media/logo.f07b8c3b.png"
+  }
+]);
